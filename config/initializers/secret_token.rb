0fad0706fcce95726ba6a47cd782c749bee9a158
Zac <mymengyu@gmail.com>
2012-11-01 00:06:49 +0800

    Initial Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boubou::Application.config.secret_token = '0c4d2d90337ee9e1c488f7f778cf971dc7ece697dc883245f83e44c696a8e7050ab986a8799ef564a7337da8fb08946ca5c7ed69cf0cdc718da58272fcb5c339'
